467b89e90a5c99e2706f47c6a0afc92f0f296995
tariqrasheed172001 <90853936+tariqrasheed172001@users.noreply.github.com>
2022-11-02 11:43:23 +0530

    updated instructors code

--- palindrome.js
+++ palindrome.js
@@ -1,16 +1,26 @@
-function palindrome(s){
-    let n = s.length;
-    let i = 0;
-    let j = n-1;
+// function palindrome(s){
+//     let n = s.length;
+//     let i = 0;
+//     let j = n-1;
 
-    while(i < j){
-        if(s[i] !== s[j]) return false;
-        i++;
-        j--;
-    }
-    return true;
+//     while(i < j){
+//         if(s[i] !== s[j]) return false;
+//         i++;
+//         j--;
+//     }
+//     return true;
 
-}
+// }
 
-console.log(palindrome("abcd"));
-console.log(palindrome("abbbbbba"));
+// console.log(palindrome("abcd"));
+// console.log(palindrome("abbbbbba"));
+
+console.log(process.argv);
+
+const str = process.argv[2];
+
+const isPalindrome = (str) => {
+    return str.split("").reverse().join("") == str;
+};
+
+console.log(isPalindrome(str));
